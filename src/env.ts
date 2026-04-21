40f98b9f65155a249b32cf76a83215540c207350
SlickYeet <lasse@lx2.dev>
2026-04-20 21:35:23 -0700

    chore: update environment variable configuration and cleanup

--- src/env.ts
+++ src/env.ts
@@ -2,50 +2,40 @@ import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
 export const env = createEnv({
-  /**
-   * Specify your client-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars. To expose them to the client, prefix them with
-   * `NEXT_PUBLIC_`.
-   */
   client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_CONTACT_EMAIL: z.email(),
     NEXT_PUBLIC_URL: z.url(),
   },
-  /**
-   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
-   * `SOME_VAR=''` will throw an error.
-   */
   emptyStringAsUndefined: true,
-
-  /**
-   * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
-   * middlewares) or client-side so we need to destruct manually.
-   */
   runtimeEnv: {
+    ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+    ADMIN_NAME: process.env.ADMIN_NAME,
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    NEXT_PUBLIC_CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL,
     NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
     NODE_ENV: process.env.NODE_ENV,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    WIREGUARD_API_ENDPOINT: process.env.WIREGUARD_API_ENDPOINT,
+    WIREGUARD_API_KEY: process.env.WIREGUARD_API_KEY,
+    WIREGUARD_VPN_ENDPOINT: process.env.WIREGUARD_VPN_ENDPOINT,
+    WIREGUARD_VPN_PORT: process.env.WIREGUARD_VPN_PORT,
   },
-  /**
-   * Specify your server-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars.
-   */
   server: {
+    ADMIN_EMAIL: z.string(),
+    ADMIN_NAME: z.string(),
+    ADMIN_PASSWORD: z.string(),
     BETTER_AUTH_SECRET: z.string(),
     DATABASE_URL: z.url(),
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_CLIENT_SECRET: z.string(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    RESEND_API_KEY: z.string(),
+    WIREGUARD_API_ENDPOINT: z.string(),
+    WIREGUARD_API_KEY: z.string(),
+    WIREGUARD_VPN_ENDPOINT: z.string(),
+    WIREGUARD_VPN_PORT: z.string(),
   },
-  /**
-   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
-   * useful for Docker builds.
-   */
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 })
