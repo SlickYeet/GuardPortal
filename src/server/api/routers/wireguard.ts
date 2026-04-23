109d8558e5b3e7ec74157a0baa3817e7180e2fe3
SlickYeet <lasse@lx2.dev>
2026-04-22 21:06:08 -0700

    feat: add getPeerConfigByUserId procedure to fetch peer configuration by user ID

--- src/server/api/routers/wireguard.ts
+++ src/server/api/routers/wireguard.ts
@@ -1,9 +1,35 @@
 import { TRPCError } from "@trpc/server"
+import { eq, getTableColumns } from "drizzle-orm"
+import * as z from "zod"
 
 import { env } from "@/env"
 import { createTRPCRouter, protectedProcedure } from "@/server/api/init"
+import { peerConfigTable, user as userTable } from "@/server/db/schema"
 
 export const wireguardRouter = createTRPCRouter({
+  getPeerConfigByUserId: protectedProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const [user] = await ctx.db
+        .select({
+          ...getTableColumns(userTable),
+          peerConfig: {
+            ...getTableColumns(peerConfigTable),
+          },
+        })
+        .from(userTable)
+        .leftJoin(peerConfigTable, eq(peerConfigTable.userId, userTable.id))
+        .where(eq(userTable.id, input.userId))
+
+      if (!user?.peerConfig) return null
+
+      return user.peerConfig
+    }),
+
   getPeerConfigs: protectedProcedure.query(async () => {
     const reqOpts: RequestInit = {
       headers: {
