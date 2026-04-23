8175d498750834f866be688567884177e9071e4f
SlickYeet <lasse@lx2.dev>
2026-04-22 19:56:07 -0700

    feat: enhance login form layout and styling for improved user experience

--- src/modules/auth/ui/login-form.tsx
+++ src/modules/auth/ui/login-form.tsx
@@ -39,7 +39,7 @@ export function LoginForm() {
 
   return (
     <Card>
-      <CardContent>
+      <CardContent className="px-8 py-6">
         <Button
           className="w-full"
           disabled={isLoading}
--- src/modules/auth/views/login.tsx
+++ src/modules/auth/views/login.tsx
@@ -5,10 +5,10 @@ import { LoginForm } from "@/modules/auth/ui/login-form"
 
 export function LoginView() {
   return (
-    <div className="flex min-h-screen items-center justify-center px-6 py-12 md:-mt-12 md:px-0 md:py-0">
-      <div className="w-full max-w-md space-y-8">
+    <div className="-mt-12 flex min-h-screen items-center justify-center px-6">
+      <div className="w-full max-w-lg space-y-8">
         <div className="text-center">
-          <span className="text-muted-foreground text-sm tracking-wide">
+          <span className="text-muted-foreground text-xs tracking-wide">
             Powered by{" "}
           </span>
           <a
@@ -18,14 +18,14 @@ export function LoginView() {
           >
             <Image
               alt="wireguard logo"
-              className="mx-auto mb-4 h-20 w-auto"
-              height={60}
+              className="mx-auto h-20 w-auto"
+              height={80}
               loading="eager"
               src="/wireguard.svg"
               width={120}
             />
           </a>
-          <h1 className="my-6 text-center font-bold text-4xl text-primary">
+          <h1 className="my-6 text-center font-bold text-3xl md:text-4xl">
             Sign in to {APP_NAME}
           </h1>
         </div>
