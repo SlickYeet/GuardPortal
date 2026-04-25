8fd5419329b02ce3da37541fdf929de0a9840a2a
SlickYeet <lasse@lx2.dev>
2026-04-24 18:42:01 -0700

    feat: refactor Layout component for improved structure and navigation

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,8 @@
+import { ArrowLeftIcon } from "lucide-react"
+import Link from "next/link"
 import { notFound } from "next/navigation"
 
+import { Button } from "@/components/ui/button"
 import { UserMenu } from "@/components/user-menu"
 import { APP_NAME } from "@/constants"
 import { isUserAdmin } from "@/helpers/is-user-admin"
@@ -15,8 +18,20 @@ export default async function Layout({ children }: LayoutProps<"/">) {
   return (
     <div className="min-h-screen px-4 py-12 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-5xl">
-        <div className="flex justify-between gap-x-4">
-          <h1 className="mb-6 font-bold text-3xl">{APP_NAME} Admin</h1>
+        <div className="@container mb-6 flex items-center justify-between gap-x-4">
+          <div className="flex items-center gap-x-2">
+            <Button
+              nativeButton={false}
+              render={<Link href="/" />}
+              size="icon-lg"
+              variant="ghost"
+            >
+              <ArrowLeftIcon className="size-6" />
+            </Button>
+            <h1 className="font-bold text-3xl">
+              <span className="@md:inline hidden">{APP_NAME}</span> Admin
+            </h1>
+          </div>
           <UserMenu isAdmin={isAdmin} user={session.user} />
         </div>
         <AdminTabs>{children}</AdminTabs>
