770ff8fedfe08de31b5605aac4fdfe19e83a2670
SlickYeet <lasse@lx2.dev>
2026-04-29 18:49:01 -0700

    feat: refactor PeerConfigsView layout for improved mobile responsiveness

--- src/modules/admin/views/peer-configs.tsx
+++ src/modules/admin/views/peer-configs.tsx
@@ -11,38 +11,48 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { useIsMobile } from "@/hooks/use-mobile"
 import { api } from "@/lib/api/client"
 import { CreatePeerConfigModal } from "@/modules/admin/ui/create-peer-config-modal"
 import { PeerConfigTable } from "@/modules/admin/ui/peer-config-table"
 
 export function PeerConfigsView() {
+  const isMobile = useIsMobile()
   const searchParams = useSearchParams()
   const peerId = searchParams.get("peerId") ?? undefined
 
   return (
-    <Card>
-      <CardHeader className="flex items-start justify-between">
-        <div className="flex flex-col gap-y-1">
-          <CardTitle>Peer Configs</CardTitle>
-          <CardDescription>
-            View and manage WireGuard peer configs.
-          </CardDescription>
+    <div className="flex flex-col justify-center pb-12">
+      <Card>
+        <CardHeader className="flex items-start justify-between">
+          <div className="flex flex-col gap-y-1">
+            <CardTitle>Peer Configs</CardTitle>
+            <CardDescription>
+              View and manage WireGuard peer configs.
+            </CardDescription>
+          </div>
+          <div className="flex items-center gap-x-2">
+            {!isMobile && peerId && <ClearPeerIdButton />}
+            <CreatePeerConfigModal />
+          </div>
+        </CardHeader>
+        <CardContent>
+          <PeerConfigTable />
+        </CardContent>
+      </Card>
+      {isMobile && peerId && (
+        <div className="fixed bottom-8 left-1/2 -translate-x-1/2">
+          <ClearPeerIdButton />
         </div>
-        <div className="flex items-center gap-x-2">
-          {peerId && <ClearPeerIdButton />}
-          <CreatePeerConfigModal />
-        </div>
-      </CardHeader>
-      <CardContent>
-        <PeerConfigTable />
-      </CardContent>
-    </Card>
+      )}
+    </div>
   )
 }
 
 function ClearPeerIdButton() {
   const router = useRouter()
   const utils = api.useUtils()
+  const isMobile = useIsMobile()
 
   function clearPeerId() {
     router.push("/admin/configs")
@@ -50,7 +60,11 @@ function ClearPeerIdButton() {
   }
 
   return (
-    <Button onClick={clearPeerId} variant="ghost">
+    <Button
+      onClick={clearPeerId}
+      size={isMobile ? "sm" : "default"}
+      variant={isMobile ? "secondary" : "ghost"}
+    >
       <XIcon /> Clear Peer ID
     </Button>
   )
